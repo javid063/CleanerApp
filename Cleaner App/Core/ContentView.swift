b3494e12c8df4f720cf32f335ef3b54738851744
Vusal <zevs.1418@gmail.com>
2024-09-11 22:41:53 +0400

    Refactoring
    - Deleted all unnecessary files and moved files to new created folders.
    - Made changes in project settings.
    - Set a display name to 'SwiftClean'

---
+++ Cleaner App/Core/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Cleaner App
+//
+//  Created by Vusal Nuriyev 2 on 11.09.24.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        ZStack {
+            
+            
+        }
+    }
+}
